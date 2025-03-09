c4f7b14d72f10689b6b4be0adf1c1b0883652544
oricake <ilovenakiri@gmail.com>
2025-03-09 21:53:28 +0900

    updates-20250309

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -60,12 +60,7 @@ const EventsPage = () => {
   const timeoutRef = useRef(null);
 
   const menuItems = [
-    { id: 'preview-nymph', text: '용광로의 부활', bgImage: '/images/event-nymph.jpg', link: 'https://gall.dcinside.com/m/mibj/5212701' },
-    { id: 'preview-roguelike-sarkaz', text: '통합전략#5 살카즈의 영원한 불가사의', bgImage: '/images/event-roguelike-sarkaz.png', link: 'https://gall.dcinside.com/m/mibj/5464489' },
-    { id: 'preview-concert-2024-ii', text: 'Ambience Synesthesia 2024 II', bgImage: '/images/event-concert-2024-ii.jpg', link: 'https://gall.dcinside.com/m/mibj/5464489' },
     { id: 'preview-dungeon-meshi', text: '테라밥', bgImage: '/images/event-dungeon-meshi.jpg', link: 'https://gall.dcinside.com/m/mibj/5436645' },
-    { id: 'preview-moon', text: '아름다운때에 달을 맞이한다', bgImage: '/images/event-moon.jpg', link: 'https://gall.dcinside.com/m/mibj/5459774' },
-    { id: 'preview-black-money-rerun', text: '부정축재 재개방', bgImage: '/images/event-black-money-rerun.jpg', link: 'https://gall.dcinside.com/m/mibj/5459920' },
     { id: 'preview-vector', text: '벡터 브레이크스루', bgImage: '/images/event-vector-breakthrough.jpg', link: 'https://gall.dcinside.com/m/mibj/5461517' },
     { id: 'preview-west', text: '해 지는 곳을 따라서', bgImage: '/images/event-west.jpg', link: 'https://gall.dcinside.com/m/mibj/5462923' },
     { id: 'preview-ggozzil', text: '벨벳의 폭로자들', bgImage: '/images/event-ggozzil.jpg', link: 'https://gall.dcinside.com/m/mibj/5464489' },
@@ -76,6 +71,11 @@ const EventsPage = () => {
     { id: 'preview-lake-rerun', text: '은심호 재개방', bgImage: '/images/event-lake-rerun.jpg', link: 'https://gall.dcinside.com/m/mibj/5464489' },
     { id: 'preview-new-year-2025', text: '2025년 신년 기념 이벤트 (CN기준)', bgImage: '/images/event-new-year-2025.jpg', link: 'https://gall.dcinside.com/m/mibj/5464489' },
     { id: 'preview-lunear-moon-2025', text: '상견환', bgImage: '/images/event-lunar-moon-2025.jpg', link: 'https://gall.dcinside.com/m/mibj/5464489' },
+    { id: 'preview-operation-atone', text: '정죄작전', bgImage: '/images/event-operation-cc-atone.jpg', link: 'https://gall.dcinside.com/m/mibj/5464489'},
+    { id: 'preview-cu-tomorow', text: '내일 다시 만나요', bgImage: '/images/event-cu-tomorrow.jpg', link: 'https://gall.dcinside.com/m/mibj/5464489'},
+    { id: 'preview-roguelike-mizuki-extension-ii', text: '통합전략#2 카에룰라 아버 확장 (2차)', bgImage: '/images/event-roguelike-mizuki-extension-ii.jpg', link: 'https://gall.dcinside.com/m/mibj/5464489'},
+    { id: 'preview-shu-rerun', text: '회서리 재개방', bgImage: '/images/event-shu-rerun.jpg', link: 'https://gall.dcinside.com/m/mibj/5464489'},
+    { id: 'preview-eblana', text: '슬픔마저 타오르고', bgImage: '/images/event-eblana.jpg', link: 'https://gall.dcinside.com/m/mibj/5464489'},
   ];
 
   const handlePointerDown = useCallback((id) => {
