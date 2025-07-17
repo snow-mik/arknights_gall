72b5ff00fac21c644aba94175a8e28c6c151a342
oricake <ilovenakiri@gmail.com>
2025-07-18 00:37:17 +0900

    restoration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/arknights-gall-info/',  // GitHub Pages에서 호스팅할 때 경로 설정
+  base: '/arknights_gall/',  // GitHub Pages에서 호스팅할 때 경로 설정
   plugins: [react()],
 })
